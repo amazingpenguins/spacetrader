008bc0da5c872500a9dce80a63aef6fc8232c8fb
scottd70192 <scottd70192@me.com>
2012-11-05 20:06:58 -0500

    Put for here star in planet button.

--- src/PlanetButton.java
+++ src/PlanetButton.java
@@ -1,11 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
 
 public class PlanetButton extends JButton {
 	private Planet planet;
 	private Dimension size;
 	protected UniversePanel up;
+	private boolean isHere; 
 
 	public PlanetButton(Planet p, Dimension size, UniversePanel up) {
 		setPreferredSize(size);
@@ -18,6 +22,22 @@ public class PlanetButton extends JButton {
 	public void paintComponent(Graphics g) {
 		this.planet.draw(g, (this.size.width - Planet.PLANET_SIZE)/2, 
 			(this.size.height - Planet.PLANET_SIZE)/2);
+		if (isHere) {
+			try {
+            	g.drawImage(ImageIO.read(new File("images/Star.jpg")),
+			 0, 0, 15, 15, null);
+        	} catch(IOException IOE) {
+            	IOE.printStackTrace();
+			}
+		}
+	}
+
+	public void setHere(boolean here) {
+		this.isHere = here;
+	}
+
+	public boolean isHere() {
+		return this.isHere;
 	}
 
 	private class PlanetButtonListener implements ActionListener {
