8958bbe7c6658079049d85260e2fb2259edadf6c
scottd70192 <scottd70192@me.com>
2012-11-05 20:17:25 -0500

    Added stuff for planet buttons.

--- src/UniversePanel.java
+++ src/UniversePanel.java
@@ -9,16 +9,19 @@ public class UniversePanel extends JPanel {
 	private SolarSystem[][] universe;
     private Player player;
     private GameController gc;
+    private PlanetButton[][] planetButtons;
 
 	public UniversePanel(SolarSystem[][] universe, GameController gc) {
         this.gc = gc;
 		setPreferredSize(new Dimension(5*TILE_SIZE, 5*TILE_SIZE));
         setLayout(new GridLayout(GameController.UNIVERSE_SIZE, GameController.UNIVERSE_SIZE));
         this.universe = universe;
+        planetButtons = new PlanetButton[GameController.UNIVERSE_SIZE][GameController.UNIVERSE_SIZE];
         for (int i = 0; i < GameController.UNIVERSE_SIZE; i++) {
             for (int j = 0; j < GameController.UNIVERSE_SIZE; j++) {
-                this.add(new PlanetButton(this.universe[i][j].getPlanet(), 
-                    new Dimension(40, 40), this));
+                planetButtons[i][j] = new PlanetButton(this.universe[i][j].getPlanet(), 
+                    new Dimension(40, 40), this);
+                this.add(planetButtons[i][j]);
             }
         }
     }
@@ -32,6 +35,9 @@ public class UniversePanel extends JPanel {
     public void goToPlanet(Planet p) {
         gc.updateMarketPanel(p);
         gc.goToState(GameController.State.MARKETPANEL);
+        // so, when you need to change the current location
+        // all you need to do is setHere on the button that corresponds to the location they are going to 
+        // then call the repaint on the button 
     }
 
 
